e45c6ca6b38bed62bed61e445b234670095fb831
Sagun Sedai <sagun.sedai@gmail.com>
2017-01-30 19:11:01 -0500

    This is the implemented version of the BeerSong.java with the updated functions

--- BeerSong.java
+++ BeerSong.java
@@ -14,15 +14,39 @@ public class BeerSong {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
+     Ninety_Nine_Bottles_of_Beer();
     }
     
     
     public static void Ninety_Nine_Bottles_of_Beer()
     {
-    
-    }
+        
+        for (int i=99; i>0; i--)
+        {
+         
+        if (i==1)
+        {
+        System.out.println(i + " bottle of beer on the wall, " + i+ " bottle of beer ");
+        System.out.println("take one down, pass it around, no more bottles of beer on the wall.");
+        }
+        
+        else if(i==2)
+        {
+        System.out.println( i+" bottles of beer on the wall, "+ i+" bottles of beer");
+        System.out.println("take one down, pass it around, " + (i-1)+ " bottle of beer on the wall. ");
+            
+        }
+            
+        else if(i>3)
+        {
+        System.out.println( i+" bottles of beer on the wall, "+ i+" bottles of beer");
+        System.out.println("take one down, pass it around, " + (i-1)+ " bottles of beer on the wall. "); 
+        }
 
+        }
+        
+       }
+    
 }
 
 
