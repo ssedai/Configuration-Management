7010e20b19aca31167a31960f59e8291b3a925bd
Sagun Sedai <sagun.sedai@gmail.com>
2017-01-30 19:08:01 -0500

    Created the empty functions in BeerSong.java

---
+++ BeerSong.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author sagun
+ */
+public class BeerSong {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+    }
+    
+    
+    public static void Ninety_Nine_Bottles_of_Beer()
+    {
+    
+    }
+
+}
+
+
